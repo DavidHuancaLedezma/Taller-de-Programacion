8d7c8235dbe0420d83baf9d892e56c161b53daf7
David <>
2023-10-03 20:56:47 -0400

    estableciendo conexion con base de datos

---
+++ src/modelo/Conexion.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author DavidH
+ */
+public class Conexion {
+    private String URL = "jdbc:mysql://localhost:3306/restauracion_tienda?autoReconnet=true&useSSL=false"; // corregir con datos originales
+    private String usuario = "root";  // usuario por defecto
+    private String password = ""; // la base de datos del xampp no tiene contraseña
+
+    public Connection getConexion() {
+        Connection conexion = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conexion = (Connection) (DriverManager.getConnection(URL, usuario, password));
+            System.out.println("Conexion exitosa");
+        } catch (Exception ex) {
+            System.err.println("Error:" + ex);
+        }
+        return conexion;
+    }
+}
