2218e65af742389b3bd6c7b0289ab75964fbb2f5
Usuario <Usuario@DESKTOP-KMC5Q8E>
2023-10-21 22:32:41 -0400

    Clase informacion Puesto

--- src/modelo/InformacionPuesto.java
+++ src/modelo/InformacionPuesto.java
@@ -11,21 +11,23 @@ import java.util.ArrayList;
 
 /**
  *
- * @author Usuario
+ * @author Erick
+ * Esta clase nos ayuda a generar los puesto de trabajo que estan libres
+ * Si desean pueden usar esta clase es facil para poder generar el combo box
  */
 public class InformacionPuesto {
  //Podemos llevar a otra clase los otros 2 
     private ResultSet resultado;
     private PreparedStatement ps; //Variable para ejecutar consultas.
     private Connection con;
-public ArrayList<Puesto> getListaPuestos(){
+public ArrayList<Puesto> getListaPuestosDisponibles(){
         ArrayList <Puesto> puestos = new ArrayList();
         Puesto puesto = null;
         
         try{
 
             con = new Conexion().getConexion();
-            ps = con.prepareStatement("select nombrepuesto from puestotrabajo");
+            ps = con.prepareStatement("select nombrepuesto from puestotrabajo \n" +"where not exists (select nombrepuesto from funcionario where puestotrabajo.idpuesto = funcionario.idpuesto) and nombrepuesto != 'Junta Directiva'");
             resultado = ps.executeQuery();
             while(resultado.next()){
                 puesto = new Puesto();
@@ -40,4 +42,27 @@ public ArrayList<Puesto> getListaPuestos(){
 
         return puestos;
     }
+/*
+El siguiente metodo lo que hace es solo realizar la consulta de todos lo puestos de trabajos
+*/
+public ArrayList<Puesto> getlistaPuesto(){
+    ArrayList <Puesto> puestos = new ArrayList();
+    Puesto puesto = null;
+    try{
+        con = new Conexion().getConexion();
+            ps = con.prepareStatement("select nombrepuesto from puestotrabajo ");
+            resultado = ps.executeQuery();
+            while(resultado.next()){
+                puesto = new Puesto();
+                puesto.setPuesto(resultado.getString("nombrepuesto"));
+                puestos.add(puesto);
+                
+            }
+        }catch(Exception ex){
+        
+        }
+    return puestos;
+    }
 }
+
+
