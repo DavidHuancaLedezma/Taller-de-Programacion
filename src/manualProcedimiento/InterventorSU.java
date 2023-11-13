6cc55039db05710a52082441f415fdb6e2e8f5c3
DrackLobo <juanjosehuanca123@gmail.com>
2023-11-13 17:15:00 -0400

    creacion de la clase InterventorSU

---
+++ src/manualProcedimiento/InterventorSU.java
@@ -0,0 +1,43 @@
+
+package manualProcedimiento;
+
+
+public class InterventorSU {
+    private int idInterventor ; 
+    private int idProcedimiento;
+    private String nombreInterventor; 
+    public InterventorSU(){
+        
+    }
+
+    public InterventorSU(int idInterventor, int idProcedimiento, String nombreInterventor) {
+        this.idInterventor = idInterventor;
+        this.idProcedimiento = idProcedimiento;
+        this.nombreInterventor = nombreInterventor;
+    }
+
+    public int getIdInterventor() {
+        return idInterventor;
+    }
+
+    public void setIdInterventor(int idInterventor) {
+        this.idInterventor = idInterventor;
+    }
+
+    public int getIdProcedimiento() {
+        return idProcedimiento;
+    }
+
+    public void setIdProcedimiento(int idProcedimiento) {
+        this.idProcedimiento = idProcedimiento;
+    }
+
+    public String getNombreInterventor() {
+        return nombreInterventor;
+    }
+
+    public void setNombreInterventor(String nombreInterventor) {
+        this.nombreInterventor = nombreInterventor;
+    }
+    
+}
